87a26fb6b32c69f1855c586bb004d9dc506c31ef
Walter Gazotto <126080076+WalterGazotto@users.noreply.github.com>
2024-04-01 12:39:34 -0300

    Create index.java

---
+++ treino/index.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class ExemploLeituraDados {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Digite um valor interiro (int): ");
+        int entradaInt = scan.nextInt();
+    }
+}

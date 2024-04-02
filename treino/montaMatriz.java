6278be67013a2f4d49a0ccbe1f60669f9367d32f
Walter Gazotto <126080076+WalterGazotto@users.noreply.github.com>
2024-04-02 12:07:13 -0300

    treino de matriz

---
+++ treino/montaMatriz.java
@@ -0,0 +1,10 @@
+public class montaMatriz {
+    public static void main(String[] args) {
+
+        String[] vet = new String[]{"Walter", "Wendy", "Álice",};
+        System.out.println("Os nome informados foram: ");
+        for(int cont = 0; cont < vet.length; cont++) {
+            System.out.println(vet[cont]);
+        }
+    }    
+}
